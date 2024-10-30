bc510acd92eb64b398e04514e16af3161ca2e166
zinebMachrouh <zinebmac49@gmail.com>
2024-10-30 17:10:21 +0100

    init project

---
+++ src/main/java/org/example/fxdatawarehouse/FxDataWarehouseApplication.java
@@ -0,0 +1,13 @@
+package org.example.fxdatawarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxDataWarehouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FxDataWarehouseApplication.class, args);
+    }
+
+}
